6e6a715c01a0e82f592b1a851b094eaf8df23a6d
gregg.drumma@gammathreetrading.com <gregg.drumma@gammathreetrading.com>
2018-07-17 13:58:30 -0400

    added batch build file.

---
+++ BatchBuild.bat
@@ -0,0 +1,30 @@
+mkdir bin\Release
+copy QuickFIXN\Bin\Release\net452\*.dll bin\Release
+copy QuickFIXN\Bin\Release\net452\*.pdb bin\Release
+
+
+copy Messages\FIX40\bin\Release\net452\QuickFix.FIX40.dll bin\Release
+copy Messages\FIX40\bin\Release\net452\QuickFix.FIX40.pdb bin\Release
+
+copy Messages\FIX41\bin\Release\net452\QuickFix.FIX41.dll bin\Release
+copy Messages\FIX41\bin\Release\net452\QuickFix.FIX41.pdb bin\Release
+
+copy Messages\FIX42\bin\Release\net452\QuickFix.FIX42.dll bin\Release
+copy Messages\FIX42\bin\Release\net452\QuickFix.FIX42.pdb bin\Release
+
+copy Messages\FIX43\bin\Release\net452\QuickFix.FIX43.dll bin\Release
+copy Messages\FIX43\bin\Release\net452\QuickFix.FIX43.pdb bin\Release
+
+copy Messages\FIX44\bin\Release\net452\QuickFix.FIX44.dll bin\Release
+copy Messages\FIX44\bin\Release\net452\QuickFix.FIX44.pdb bin\Release
+
+copy Messages\FIX50\bin\Release\net452\QuickFix.FIX50.dll bin\Release
+copy Messages\FIX50\bin\Release\net452\QuickFix.FIX50.pdb bin\Release
+
+copy Messages\FIX50SP1\bin\Release\net452\QuickFix.FIX50SP1.dll bin\Release
+copy Messages\FIX50SP1\bin\Release\net452\QuickFix.FIX50SP1.pdb bin\Release
+
+copy Messages\FIX50SP2\bin\Release\net452\QuickFix.FIX50SP2.dll bin\Release
+copy Messages\FIX50SP2\bin\Release\net452\QuickFix.FIX50SP2.pdb bin\Release
+
+pause
