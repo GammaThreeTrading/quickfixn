16273ce95ceaab16c15cac3b4c9c9f61573ffefe
Gregg Drumma <gregg.drumma@gammathreetrading.com>
2025-11-20 12:20:46 -0500

    This time update so that IgnoreAdminMessage in SQLStore is the default.  No need to store Admin as we would never resend those in a resend request.

--- QuickFIXn/SessionSettings.cs
+++ QuickFIXn/SessionSettings.cs
@@ -96,7 +96,7 @@ namespace QuickFix
         public const string SQL_LOG_EVENT_TABLE = "SQLLogEventTable";
         public const string SQL_LOG_EVENT_BACKUP_TABLE = "SQLLogEventBackupTable";
         public const string SQL_STORE_SESSION_TABLE = "SQLStoreSessionTable";
-        public const string SQL_STORE_IGNORE_HEARTBEATS = "SQLStoreIgnoreHeartbeats";
+        public const string SQL_STORE_IGNORE_ADMIN_MESSAGES = "SQLStoreIgnoreAdminMessages";
         public const string SQL_STORE_MESSAGES_TABLE = "SQLStoreMessagesTable";
         public const string SQL_STORE_DATASOURCE = "SQLStoreDatasource";
         public const string SQL_STORE_INITIAL_CATALOG = "SQLStoreInitialCatalog";
